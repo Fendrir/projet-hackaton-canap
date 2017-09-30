9e8b09e5ee4690997e68a5eedb6de86d11815ccd
LegrandMichael <michael.c.legrand@gmail.com>
2017-10-01 01:59:30 +0200

    MAJ home, présentation

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,38 @@
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class=""></div>
+        <div class="col-md-8 col-md-offset-4">
+            <legend><h3>Je veux co-worker...</h3></legend>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-5"><img src="img/Campagne.jpg" alt="campagne"></div>
+        <div class="col-md-5 col-md-offset-2"><img src="img/ville.jpg" alt="ville"></div>
+        <div class="col-md-5"><img src="img/mer.jpg" alt="mer"></div>
+        <div class="col-md-5 col-md-offset-2"><img src="img/montagne.jpg" alt="montagne"></div>
+    </div>
+    <div class="row">
+      
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <div id="About">
+                <h4>A propos de nous :</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nullam dapibus lectus sit amet ex iaculis tincidunt. 
+                    In ex dui, auctor.</p>
+            </div>
+        </div>
+        <div id="border-left" class="col-md-4">
+            <h4>Pourquoi faire du coworking ?</h4>
+            <p>Le coworking, cotravail en français
+            ou parfois bureaux partagés est un type d'organisation du 
+            travail qui regroupe deux notions : un espace de travail partagé,
+            mais aussi un réseau de travailleurs encourageant l'échange et l'ouverture. 
+            Il est un des domaines de l'économie collaborative.
+            Cela permet aux travailleurs du numérique d'échanger des procédés et des 
+            compétences de travail voire même des méthodes. Le co-working est au numérique ce que
+            le principe d'échange équivalent est à l'alchimie.</p>
+        </div>
+        
     </div>
 </div>
