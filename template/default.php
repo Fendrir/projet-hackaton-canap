c6ad85633c2fe1bc61aaa01484087ff6595a8063
LegrandMichael <michael.c.legrand@gmail.com>
2017-09-30 21:01:30 +0200

    Modification du home ajout présentation

--- template/default.php
+++ template/default.php
@@ -65,26 +65,26 @@
             <br>
               <div class="col-md-4">
                 <center>
-                  <img src="http://i39.tinypic.com/119lz43.jpg" class="img-circle" alt="the-brains">
+                  <img src="img/coworker.jpg" class="img-circle" alt="coworker">
                   <br>
                   <h4 class="footertext">Coworker</h4>
-                  <p class="footertext">Et si vous pensiez à travailler tout en vous ressourcant ?<br>
+                  <p class="footertext">Et si vous pensiez à travailler autrement ?<br>
                 </center>
               </div>
               <div class="col-md-4">
                 <center>
-                  <img src="http://oi60.tinypic.com/2z7enpc.jpg" class="img-circle" alt="...">
+                  <img src="img/skills.jpg" class="img-circle" alt="compétences">
                   <br>
-                  <h4 class="footertext">Artist</h4>
-                  <p class="footertext">All the images here are hand drawn by this man.<br>
+                  <h4 class="footertext">Skills</h4>
+                  <p class="footertext">Venez partager vos compétences.<br>
                 </center>
               </div>
               <div class="col-md-4">
                 <center>
-                  <img src="http://oi61.tinypic.com/307n6ux.jpg" class="img-circle" alt="...">
+                  <img src="img/sofa.jpg" class="img-circle" alt="...">
                   <br>
-                  <h4 class="footertext">Designer</h4>
-                  <p class="footertext">This pretty site and the copy it holds are all thanks to this guy.<br>
+                  <h4 class="footertext">Sofa</h4>
+                  <p class="footertext">Venez vous ressourcer chez d'autres coworker.<br>
                 </center>
               </div>
             </div>
